9655657960161e54fef5b9d3075fa9f9723697a0
yuta kanehara <yutakanehara@yuta-no-MacBook-Pro.local>
2019-02-17 11:16:58 +0900

    feat: add module logrus

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yukagil/sample-go-app
+
+require github.com/sirupsen/logrus v1.3.0
--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+
 	http.HandleFunc("/", handler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+
+	logrus.WithFields(logrus.Fields{
+		"port": port,
+	}).Info("Listening port")
 
 	http.ListenAndServe(":"+port, nil)
 }
